d2f2084a5f8d1d63c25b3b8aa2ca9f05e5dfbba7
eunseo1030 <eunseo20001030@gmail.com>
2024-11-11 14:16:02 +0900

    PostgreSQL로 변경

--- src/main/java/com/koreait/surl_project_11_08/domain/surl/surl/repository/SurlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koreait.surl_project_11_08.domain.surl.surl.repository;
-
-import com.koreait.surl_project_11_08.domain.surl.surl.entity.Surl;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface SurlRepository extends JpaRepository<Surl, Long> {
-
-}
--- src/main/java/com/koreait/surl_project_11_08/standard/dto/Empty.java
+++
@@ -1,4 +0,0 @@
-package com.koreait.surl_project_11_08.standard.dto;
-
-public class Empty {
-}
